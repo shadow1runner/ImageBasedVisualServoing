68493f3596fe415ddb6583086b8856faae525f3d
Helmut Wolf <helmut.wolf@outlook.com>
2013-04-29 17:36:16 +0200

    Fixed project properties, now builds on HELMUT-PC added eclipe gitignore-fails aso https://github.com/github/gitignore/blob/master/Global/Eclipse.gitignore

--- OpenCV Library - 2.4.4/gen/org/opencv/engine/OpenCVEngineInterface.java
+++ OpenCV Library - 2.4.4/gen/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: D:\\Helmut Wolf\\Skydrive\\Studium\\Einführung in autonome und intelligente Systeme\\PS\\3.)ImageBasedVisualServoing\\OpenCV Library - 2.4.4\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: D:\\Helmut Wolf\\Skydrive\\Studium\\Einführung in autonome und intelligente Systeme\\PS\\4.)ImageBasedVisualServoing\\OpenCV Library - 2.4.4\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
