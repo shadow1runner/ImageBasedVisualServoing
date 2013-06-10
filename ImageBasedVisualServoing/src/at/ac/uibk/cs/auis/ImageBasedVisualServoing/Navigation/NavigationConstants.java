f3bcdeb3d316d5c123a66121f2f6e472ff0f9359
Helmut Wolf <helmut.wolf@outlook.com>
2013-06-10 19:26:55 +0200

    finished second work-task: localization

--- ImageBasedVisualServoing/src/at/ac/uibk/cs/auis/ImageBasedVisualServoing/Navigation/NavigationConstants.java
+++ ImageBasedVisualServoing/src/at/ac/uibk/cs/auis/ImageBasedVisualServoing/Navigation/NavigationConstants.java
@@ -3,5 +3,5 @@ package at.ac.uibk.cs.auis.ImageBasedVisualServoing.Navigation;
 import org.opencv.core.Point;
 
 public class NavigationConstants {
-	public static Point GoalLocationWorldCoordinates = new Point(100.0, 0.0); 
+	public static Point GoalLocationWorldCoordinates = new Point(20.0, 50.0); 
 }
